6672a9d17108d4dfa1193253743aee571d5f17f5
choton654 <choton654@gmail.com>
2021-01-31 12:10:40 +0530

    xx

--- screans/category/category.js
+++ screans/category/category.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Menu, Divider, Button } from "react-native-paper";
 import axios from "axios";
 import BASE_URL from "../../api";
 import { CategoryContext } from "./categorycontext";
 const Category = ({route,navigation}) => {
   const {catId} = route.params;
   const { catstate, catdispatch } = useContext(CategoryContext);
+  const [visible, setVisible] = React.useState(false);
+  
+  const openMenu = () => setVisible(true);
+  
+  const closeMenu = () => setVisible(false);
   useEffect(() => {
     getAllCategory();
   }, []);
@@ -19,15 +25,24 @@ const Category = ({route,navigation}) => {
       .catch((err) => console.log(err));
   };
   return (
-    <View>
+    <View style={{flex:1}}>
       {catstate.categories.length !== 0 ? (
-        <View>
-          <Text>From Categoty</Text>
+        <View style={{flex:1, flexDirection:"row"}}>
           {catstate.categories.map(
-            (cat) => cat.parentId && cat.parentId._id.toString() === catId.toString() && <Text>{cat.name}</Text>
+            (cat) => cat.parentId && cat.parentId._id.toString() === catId.toString() && 
+          <View style={{flex:1, flexDirection:"row"}}>
+           
+            <Menu
+              visible={visible}
+              onDismiss={closeMenu}
+              anchor={<Button onPress={openMenu}>{cat.name}</Button>}>
+              <Menu.Item onPress={() => {}} title="Item 1" />
+              <Menu.Item onPress={() => {}} title="Item 2" />
+              <Divider />
+              <Menu.Item onPress={() => {}} title="Item 3" />
+            </Menu>
+          </View>
           ) 
-          // : <Text>No categories</Text>
-            
           }
         </View>
       ) : (
