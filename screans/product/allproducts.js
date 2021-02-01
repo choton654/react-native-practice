96dbe81130718c5ed438b19a95a8708174e274d9
choton654 <choton654@gmail.com>
2021-02-01 12:50:37 +0530

    gg

--- screans/product/allproducts.js
+++ screans/product/allproducts.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React,{useContext} from "react";
 import { View, Text } from "react-native";
+import { List } from "react-native-paper";
+import { CategoryContext } from "../category/categorycontext";
 
 const Allproducts = ({route, navigation}) => {
+  const { catstate, catdispatch } = useContext(CategoryContext);
+  const [expanded, setExpanded] = React.useState(true);
   
+  const handlePress = () => setExpanded(!expanded)
   return (
-    <View>
-      <Text>From allproducts {route.params.subCatid}</Text>
+    <View style={{flex:1, flexDirection:"column"}}>
+     <List.Section title="Choose your products">
+           <List.Accordion
+             title="Controlled Accordion"
+             left={props => <List.Icon {...props} icon="folder" />}
+             expanded={expanded}
+             onPress={handlePress}>
+             {catstate.categories.map((cat)=> cat.parentId && cat.parentId._id.toString() === route.params.subCatid.toString() && 
+             <List.Item title={cat.name} />
+             )}
+           </List.Accordion>
+         </List.Section>
     </View>
   );
 };
