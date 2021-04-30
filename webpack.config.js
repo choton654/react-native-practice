faf8a6c2d31b3d9ce7f9a3ea5d06dae934bcade3
choton654 <choton654@gmail.com>
2021-04-30 22:48:54 +0530

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const createExpoWebpackConfigAsync = require("@expo/webpack-config");
-
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(
     {
@@ -9,6 +9,16 @@ module.exports = async function (env, argv) {
     },
     argv
   );
-  // Customize the config before returning it.
+
+  // Optionally you can enable the bundle size report.
+  // It's best to do this only with production builds because it will add noticeably more time to your builds and reloads.
+  if (env.mode === "production") {
+    config.plugins.push(
+      new BundleAnalyzerPlugin({
+        path: "web-report",
+      })
+    );
+  }
+
   return config;
 };
